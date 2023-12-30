f19313bbeba765969e517d90bd22479d03022f93
Gramet Maximiliano <maxigramet03@gmail.com>
2023-12-30 14:38:20 -0300

    Dao Reparado y completo

--- src/daos/dbManager/cart.dao.js
+++ src/daos/dbManager/cart.dao.js
@@ -1,4 +1,5 @@
 import { CartModel } from "../../models/cart.model.js";
+import {productModel} from "../../models/product.model.js";
 
 class CartDao {
   async findCart() {
@@ -21,6 +22,154 @@ class CartDao {
     await CartModel.deleteMany({ author: _id });
 
     return await CartModel.findByIdAndDelete({ _id });
+  }
+
+  //* Preentrega 2
+
+  async getCartProduct(id) {
+    try {
+      const cart = await CartModel.findById(id).lean();
+
+      if (!cart) {
+        return null;
+      }
+
+      cart.products = await Promise.all(
+        cart.products.map(async (product) => {
+          try {
+            const detailedProduct = await productModel.findById(product.product);
+            return { product: detailedProduct, quantity: product.quantity };
+          } catch (error) {
+            console.error(`Error at getting product details: ${error.message}`);
+            return null;
+          }
+        })
+      );
+
+      return cart;
+    } catch (error) {
+      console.error(`Error at getting the cart: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async addProductCart(cartId, productId) {
+    try {
+      const cart = await CartModel.findById(cartId);
+      if (!cart) {
+        throw new Error('Cart not found');
+      }
+  
+      const product = await productModel.findById(productId);
+      if (!product) {
+        throw new Error('Product not found');
+      }
+  
+      const productExist = cart.products.find((p) => p._id.equals(productId));
+  
+      if (productExist) {
+        productExist.quantity += 1;
+      } else {
+        cart.products.push({
+          _id: productId,
+          quantity: 1,
+        });
+      }
+
+      await cart.save();
+  
+      return 'Product added to the cart';
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async setProductQuantity(cartId, productId, newQuantity) {
+    try {
+      const cart = await CartModel.findById(cartId);
+  
+      if (!cart) {
+        throw new Error('Cart not found');
+      }
+      const product = cart.products.find((p) => p._id.equals(productId));
+  
+      if (!product) {
+        throw new Error('Product not found in the cart');
+      }
+  
+      product.quantity = newQuantity;
+  
+      await cart.save();
+  
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteProductFromCart(cartId, productId) {
+    try {
+      const cart = await CartModel.findById(cartId);
+  
+      if (!cart) {
+        throw new Error('Cart not found');
+      }
+  
+      const initialProductCount = cart.products.length;
+  
+      cart.products = cart.products.filter((p) => !p._id.equals(productId));
+      if (cart.products.length === initialProductCount) {
+        throw new Error('Product not found in the cart');
+      }
+  
+      await cart.save();
+  
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async setCartProducts(cartId, newProducts) {
+    try {
+      const cart = await CartModel.findById(cartId);
+  
+      if (!cart) {
+        throw new Error('Cart not found');
+      }
+  
+      cart.products = [];
+
+      newProducts.forEach(product => {
+        cart.products.push({
+          product: product.productId,
+          quantity: product.quantity
+        });
+      });
+  
+      await cart.save();
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async clearCart(cartId) {
+    try {
+      const cart = await CartModel.findById(cartId);
+
+      if (!cart) {
+        throw new Error('Cart not found');
+      }
+
+      cart.products = [];
+ 
+      await cart.save();
+  
+      return cart;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
